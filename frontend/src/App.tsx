5a0e1f378723887c10d5a56ca575993493ab724f
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-19 23:07:49 +0800

    added chat layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './App.css'
+import { MessageBox } from './components/MessageBox'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-red-500">Hello</h1>
-    </>
+    <div className="min-h-screen">
+      <div className="p-4">
+        <div className="bg-gray-700 border border-gray-500 rounded-md p-2 w-full h-[600px]">
+          <MessageBox />
+        </div>  
+        <input type="text" className="w-full p-2 rounded-md mt-4" placeholder="Type here..." />
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/components/MessageBox.tsx
@@ -0,0 +1,8 @@
+
+export const MessageBox = () => {
+  return (
+    <div className="p-2 bg-green-500 rounded-md max-w-[400px] ml-auto mb-2">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, tenetur!</p>
+    </div>
+  )
+}
