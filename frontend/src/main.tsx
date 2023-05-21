acb1911fff621772e2c1794be492d9ce96b6c3f7
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-21 20:10:03 +0800

    add register layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Register } from './pages/register.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,11 @@
+
+export const Register = () => {
+  return (
+    <div className="min-h-screen grid place-items-center">
+      <div>
+        <h1 className="text-4xl mb-4">Chat App</h1>
+        <button>Google Login</button>
+      </div>
+    </div>
+  )
+}
