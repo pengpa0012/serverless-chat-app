a52895ab8908c39038a00e6bbfe5fbf553f23692
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-21 09:00:00 +0800

    added message box

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import { MessageBox } from './components/MessageBox'
 
 function App() {
+  const [messages, setMessages] = useState<any[]>([])
+  const input = useRef<any>()
+
+  const handleType = (e: any) => {
+    const text = e.target.value
+    if(e.key != "Enter" || !text || /^\s*$/.test(text)) return
+    setMessages([...messages, text])
+    input.current.value = ""
+  }
 
   return (
     <div className="min-h-screen">
       <div className="p-4">
-        <div className="bg-gray-700 border border-gray-500 rounded-md p-2 w-full h-[600px]">
-          <MessageBox />
+        <div className="bg-gray-700 border border-gray-500 rounded-md p-2 w-full h-[600px] overflow-y-scroll">
+          {
+            messages.map(message => (
+              <MessageBox text={message} />
+            ))
+          }
         </div>  
-        <input type="text" className="w-full p-2 rounded-md mt-4" placeholder="Type here..." />
+        <input type="text" className="w-full p-2 rounded-md mt-4" placeholder="Type here..." ref={input} onKeyDown={handleType} />
       </div>
     </div>
   )
--- frontend/src/components/MessageBox.tsx
+++ frontend/src/components/MessageBox.tsx
@@ -1,8 +1,8 @@
 
-export const MessageBox = () => {
+export const MessageBox = ({text}: any) => {
   return (
-    <div className="p-2 bg-green-500 rounded-md max-w-[400px] ml-auto mb-2">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, tenetur!</p>
+    <div className="text-start p-2 bg-green-500 rounded-md max-w-[400px] ml-auto mb-2 break-all">
+      <p>{text}</p>
     </div>
   )
 }
