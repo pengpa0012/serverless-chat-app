7cf23eeb1903aec444191354a458c392f20640af
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-23 07:10:45 +0800

    add registerUser function

---
+++ backend/function/registerUser.js
@@ -0,0 +1,50 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+const userTableName = 'chatAppTable';
+
+exports.handler = async (event) => {
+  const { username, password, bio, age, name } = event;
+
+  // Generate a unique ID for the user
+  const userId = uuidv4();
+
+  // 1. Register the user in Cognito User Pools
+  const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+  const cognitoParams = {
+    UserPoolId: process.env.USER_POOL_ID,
+    Username: username,
+    TemporaryPassword: password,
+  };
+
+  try {
+    await cognitoIdentityServiceProvider.adminCreateUser(cognitoParams).promise();
+  } catch (error) {
+    console.log('Error registering user in Cognito:', error);
+    throw new Error('Failed to register user');
+  }
+
+  // 2. Save the user data to DynamoDB
+  const userItem = {
+    userId,
+    username
+  };
+
+  const dynamoParams = {
+    TableName: userTableName,
+    Item: userItem,
+  };
+
+  try {
+    await dynamoDB.put(dynamoParams).promise();
+  } catch (error) {
+    console.log('Error saving user data to DynamoDB:', error);
+    throw new Error('Failed to save user data');
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'User registered successfully', userItem }),
+  };
+};
