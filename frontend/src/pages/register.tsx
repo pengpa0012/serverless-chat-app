58c62712e5d95c7f775415557f041e68e6131185
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-24 00:05:26 +0800

    add form layout

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -2,9 +2,16 @@
 export const Register = () => {
   return (
     <div className="min-h-screen grid place-items-center">
-      <div>
-        <h1 className="text-4xl mb-4">Chat App</h1>
-        <div className="g-signin2" data-longtitle="true"></div>
+      <div className="bg-gray-700 rounded-md p-6">
+        <ul className="flex">
+          <li>Signup</li>
+          <li>Login</li>
+        </ul>
+        <form className="flex flex-col gap-4">
+          <input type="text" placeholder="username" className="p-2 outline-none rounded-md" />
+          <input type="password" placeholder="password" className="p-2 outline-none rounded-md" />
+          <button type="submit">Sign up</button>
+        </form>
       </div>
     </div>
   )
