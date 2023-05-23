9e981d70b4f9d4152eeb6b3bc0a47bb2a757c452
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-24 07:35:54 +0800

    add inputs

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -2,6 +2,11 @@ import { useState } from "react"
 
 export const Register = () => {
   const [tab, setTab] = useState(0)
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+  }
+
   return (
     <div className="min-h-screen grid place-items-center">
       <div className="bg-[#3b393e] rounded-[30px] p-6 w-[350px] md:w-[500px]">
@@ -9,10 +14,22 @@ export const Register = () => {
           <li className={`hover:bg-white/10 w-full p-3 cursor-pointer ${tab == 0 && "font-bold border border-white border-r-0 border-l-0 border-t-0"}`} onClick={() => setTab(0)}>Signup</li>
           <li className={`hover:bg-white/10 w-full p-3 cursor-pointer ${tab == 1 && "font-bold border border-white border-r-0 border-l-0 border-t-0"}`} onClick={() => setTab(1)}>Login</li>
         </ul>
-        <form className="flex flex-col gap-4 py-4">
-          <input type="text" placeholder="username" className="p-2 outline-none rounded-md bg-[#5a5a5a]" />
-          <input type="password" placeholder="password" className="p-2 outline-none rounded-md bg-[#5a5a5a]" />
-          <button type="submit" className="bg-[#3862c6]">Sign up</button>
+        <form className="flex flex-col gap-4 py-4" onSubmit={handleSubmit}>
+          {
+            tab == 0 ?
+            <>
+              <input type="text" placeholder="username" className="p-2 outline-none rounded-md bg-[#5a5a5a]" />
+              <input type="password" placeholder="password" className="p-2 outline-none rounded-md bg-[#5a5a5a]" />
+              <input type="password" placeholder="repeat password" className="p-2 outline-none rounded-md bg-[#5a5a5a]" />
+              <button type="submit" className="bg-[#3862c6]">Sign up</button>
+            </>
+            :
+            <>
+              <input type="text" placeholder="username" className="p-2 outline-none rounded-md bg-[#5a5a5a]" />
+              <input type="password" placeholder="password" className="p-2 outline-none rounded-md bg-[#5a5a5a]" />
+              <button type="submit" className="bg-[#3862c6]">Log in</button>
+            </>
+          }
         </form>
       </div>
     </div>
